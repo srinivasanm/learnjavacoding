bdcca236224de267329b4708f794c34659b85267
srinivasanm <srinivasan253@gmail.com>
2017-10-20 21:31:46 +0530

    spring boot tracing base code

---
+++ spring-boot/src/main/java/com/tryouts/springtracing/SpringTracingApplication.java
@@ -0,0 +1,12 @@
+package com.tryouts.springtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTracingApplication.class, args);
+	}
+}
